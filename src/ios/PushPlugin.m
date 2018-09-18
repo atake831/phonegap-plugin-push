f3a86209949cd8827e7dfb0697affa98cf38925a
Akira Takeguchi <a.takeguchi.sf@gmail.com>
2018-09-18 18:31:54 +0900

    remove no-need if branch

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -293,23 +293,13 @@
         NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:path];
         fcmSenderId = [dict objectForKey:@"GCM_SENDER_ID"];
         BOOL isGcmEnabled = false;
-      
+
         NSLog(@"FCM Sender ID %@", fcmSenderId);
 
         //  GCM options
         [self setFcmSenderId: fcmSenderId];
-        if(isGcmEnabled && [[self fcmSenderId] length] > 0) {
-            NSLog(@"Using FCM Notification");
-            [self setUsesFCM: YES];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                if([FIRApp defaultApp] == nil)
-                    [FIRApp configure];
-                [self initRegistration];
-            });
-        } else {
-            NSLog(@"Using APNS Notification");
-            [self setUsesFCM:NO];
-        }
+        NSLog(@"Using APNS Notification");
+        [self setUsesFCM:NO];
         id fcmSandboxArg = [iosOptions objectForKey:@"fcmSandbox"];
 
         [self setFcmSandbox:@NO];
